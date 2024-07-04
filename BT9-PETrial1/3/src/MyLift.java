7668bf7c5ea334ba0aebd33883dc7fbd40abd965
TomOutfit <167419245+TomOutfit@users.noreply.github.com>
2024-07-04 22:01:01 +0700

    Update MyLift.java

--- BT9-PETrial1/3/src/MyLift.java
+++ BT9-PETrial1/3/src/MyLift.java
@@ -9,14 +9,18 @@ public class MyLift implements ILift{
     }
 
     @Override
-    public void f2(List<Lift> t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public void f2(List<Lift> t) {
+        if (t.size()<5) {
+            System.out.println("Can't reverse");   ;
+        } else {
+            Collections.reverse(t.subList(0, 5)); //Collections.reverse -> Đảo ngược các phần tử đầu và giữ nguyên phần còn lại
+        }
     }
 
     @Override
     public void f3(List<Lift> t) {
         if (t.size() > 1 && t.get(2).getLoad() < 20) {
-            t.remove(2);
+            t.remove(2); //t.remove -> Xóa phần tử của t
         }
     }
 
